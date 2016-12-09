90322fd5f3acda594754d0e09e238d519fd9280e
DAM <larry.a_sanchez@hotmail.es>
2016-12-09 21:04:48 +0100

    y

--- src/main/java/com/example/repository/AtletaRepository.java
+++ src/main/java/com/example/repository/AtletaRepository.java
@@ -3,9 +3,11 @@ package com.example.repository;
 import com.example.domain.Atleta;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 /**
  * Created by DAM on 1/12/16.
  */
 public interface AtletaRepository  extends JpaRepository<Atleta,Long>{
-
+    List<Atleta> findByNombreContain(String nombre);
 }
--- src/main/java/com/example/repository/MedallaRepository.java
+++ src/main/java/com/example/repository/MedallaRepository.java
@@ -1,9 +1,13 @@
 package com.example.repository;
 
+import com.example.domain.Medalla;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 
 /**
  * Created by DAM on 1/12/16.
  */
-public interface MedallaRepository extends JpaRepository {
+public interface MedallaRepository extends JpaRepository <Medalla,Long>{
+    List<Medalla> finByCompeti (String nombre);
 }
